a5df32bf505d5fa3b38a4ca4c4b13a6d3cc53dfb
Epsilon2 <tobias.baumann@uni-ulm.de>
2013-12-13 11:24:59 +0100

    Update Main.java

--- Ex-5.2/src/Main.java
+++ Ex-5.2/src/Main.java
@@ -1,8 +1,8 @@
-
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Resolver res = new Resolver(args[0]);
+		res.isInconsistent(args[1]);
 
 	}
 
